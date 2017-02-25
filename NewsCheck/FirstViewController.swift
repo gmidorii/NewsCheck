01b7dd266c26f8247e0b0cbcf2e1f911f110ce71
tagasouichiro <stitow14@gmail.com>
2017-02-25 13:30:53 +0900

    Initial Commit

---
+++ NewsCheck/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NewsCheck
+//
+//  Created by taga soichiro on 2017/02/25.
+//  Copyright © 2017年 taga soichiro. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
